9bcb3fbead1edc6319d43a6080882ccee15ca03a
jamalbendadi <jamal.bendadi@student.kdg.be>
2024-10-21 12:26:02 +0000

    better mobile interaction + bugfix: wrong country data shown

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Map, { Source, Layer } from 'react-map-gl';
+import Map, { Source, Layer, ViewStateChangeEvent } from 'react-map-gl';
 import { dataLayer } from '@/lib/map-style'
 import { ApiService } from '@/infrastructure/ApiService';
 import africa_shape from '@/assets/africa_shapes/africa_shape_new.json';
@@ -15,22 +15,28 @@ function App() {
     zoom: 2.5
   });
   const [hoverInfo, setHoverInfo] = React.useState<HoverInfo | null>(null);
-
+  const currentIso = React.useRef<string>("");
+  const onMove = React.useCallback(async (event: ViewStateChangeEvent) => {
+    setHoverInfo(null)
+    setViewState(event.viewState)
+  }, [])
 
   const onHover = React.useCallback(async (event: any) => {
-    const {
-      features,
-      point: { x, y }
-    } = event;
+    const { features, point: { x, y } } = event;
     const hoveredFeature = features && features[0];
     if (!hoveredFeature) {
       setHoverInfo(null);
       return;
     }
     const iso = hoveredFeature.properties.iso_a3;
+    currentIso.current = iso;
     try {
-      const metrics = await apiService.getMetrics(iso);
-      const phaseData = await apiService.getIPCPeaks(2024, iso);
+      const [metrics, phaseData] = await Promise.all([
+        apiService.getMetrics(iso),
+        apiService.getIPCPeaks(2024, iso)
+      ]);
+
+      if(currentIso.current !== iso) {return};
       setHoverInfo({ feature: hoveredFeature, x, y, metrics, phaseData });
     }
     catch (e) {
@@ -45,9 +51,10 @@ function App() {
       <Map reuseMaps
         mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
         {...viewState}
-        onMove={evt => setViewState(evt.viewState)}
+        onMove={onMove}
         mapStyle="mapbox://styles/mapbox/streets-v11"
         onMouseMove={onHover}
+        onTouchEnd={onHover}
         interactiveLayerIds={['data']}
 
       >
